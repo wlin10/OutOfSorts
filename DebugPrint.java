4a9b5e88dfce252d467de449d3e70b5530c7055f
wlin10 <wlin10@stuy.edu>
2020-12-16 21:09:40 -0500

    base

---
+++ DebugPrint.java
@@ -0,0 +1,20 @@
+public class DebugPrint{
+  //TOGGLE THIS VARIABLE TO TURN ON/OFF prints
+  public static boolean DEBUG = true;
+
+  public static void dprint(String s){
+    if(DEBUG){
+      System.out.println(s);
+    }
+  }
+
+  //Add more methods.
+
+  public static void main(String[]args){
+    //do this so you do not forget to turn it off
+    dprint("TURN THIS OFF BEFORE SUBMITTING YOUR CODE!!!!!!!!");
+
+    //Add the other parts of your main
+
+  }
+}
