aed94e24792c213cfc8a427e474a89232d1a35a7
wlin10 <wlin10@stuy.edu>
2020-12-18 00:04:54 -0500

    newArray

--- Tester.java
+++ Tester.java
@@ -3,6 +3,15 @@ import java.util.Arrays;
 
 public class Tester{
 
+  public static int[] newArray(int sizeOfArray) {
+    int[] newArray = new int[sizeOfArray];
+    Random rng = new Random();
+    for(int i =  0; i < sizeOfArray; i++ ){
+      newArray[i] = (rng.nextInt() % 1000);
+    }
+    return newArray;
+  }
+
 
   public static void main(String[]args){
 
