245404b1516603348009a09ae7b6fba297c68399
wlin10 <wlin10@stuy.edu>
2020-12-18 10:09:07 -0500

    more tests

--- Tester.java
+++ Tester.java
@@ -19,6 +19,26 @@ public class Tester{
     return (Arrays.equals(arr1, arr2));
   }
 
+  public static void reverse(int[] arr) {
+    int[] arr1 = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      arr1[i] = arr[arr.length - i - 1];
+    }
+    arr = arr1;
+  }
+
+  public static boolean reverseArrayChecker(int seedValue, int sizeOfArray) {
+    int[] arr1 = newArray(seedValue, sizeOfArray);
+    Sorts.bubbleSort(arr1);
+    reverse(arr1);
+    Arrays.sort(arr1);
+    int[] arr2 = newArray(seedValue, sizeOfArray);
+    Arrays.sort(arr2);
+    reverse(arr2);
+    Sorts.bubbleSort(arr2);
+    return (Arrays.equals(arr1, arr2));
+  }
+
 
   public static void main(String[]args){
     Random x = new Random();
@@ -28,8 +48,22 @@ public class Tester{
         if (!arrayChecker(i, j)) {
           System.out.println("Tests Failed");
         }
+        if (!reverseArrayChecker(i, j)) {
+          System.out.println("Tests Failed");
+        }
       }
     }
+
+    int[] a = {1, 1, 3, 1, 2, 5};
+    int[] b = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int[] c = {};
+    Sorts.bubbleSort(a);
+    Sorts.bubbleSort(b);
+    Sorts.bubbleSort(c);
+    System.out.println(Arrays.toString(a));
+    System.out.println(Arrays.toString(b));
+    System.out.println(Arrays.toString(c));
+
     System.out.println("Tests Passed");
   }
 }
